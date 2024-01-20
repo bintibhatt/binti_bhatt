7bf2d91b3034099e235cdf7b97585c1de48db887
Binti Bhatt <bintijbhatt@gmail.com>
2024-01-20 19:48:04 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Footer from "./Footer";
 
 const Hero = () => {
   return (
@@ -36,6 +37,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
+    <Footer/>
     </section>
   );
 };
